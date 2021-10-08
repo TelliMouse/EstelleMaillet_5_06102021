6d754124094710ee5b15c2f98be4f7f56dfde887
Estelle Maillet <estellemaillet00@gmail.com>
2021-10-08 17:59:20 +0200

    Débogage de l ajout de produit dans localStorage depuis la page produit

--- front/js/product.js
+++ front/js/product.js
@@ -98,12 +98,153 @@ const createProductInfo = product => {
 };
 
 
+//_____________________________________________
+
+
+let productColor;
+let productQuantity;
+
+
+const getCurrentProduct = () => {
+
+    let currentProduct = {
+
+        name: document.getElementById('title').innerText,
+        color: productColor,
+        quantity: productQuantity
+    };
+
+    return currentProduct;
+};
+
+
+const isTheProductComplete = () => {
+
+    const product = getCurrentProduct();
+
+    if(product.name && product.color && product.quantity !== 0) {
+
+        return true;
+
+    } else {
+
+        return false;
+    };
+};
+
+
+const findTheRankOfTheProductInStorage = product => {
+
+    for( let i = 0; i < localStorage.length; i++){
+
+        const productRank = i + 1;
+        const productInStorage = localStorage.getItem(`Product${productRank}`);
+        const productInStorageJson = JSON.parse(productInStorage);
+
+        if(product.name == productInStorageJson.name && product.color == productInStorageJson.color) {
+            
+            return productRank;
+        };
+    };
+};
+
+
+const isTheProductAlreadyInTheCartAndWhatIsTheRank = product => {
+
+    if(localStorage.length !== 0) {
+
+        const index = findTheRankOfTheProductInStorage(product);
+
+        return index;
+
+    } else {
+
+        return false;
+    };
+};
+
+
+const getProductJsonFromStorage = rank => {
+
+    return JSON.parse(localStorage.getItem(`Product${rank}`));
+};
+
+
+const addNumbersFromString = (string1, string2) => {
+
+    return parseInt(string1, 10) + parseInt(string2, 10);
+};
+
+
+const replaceObjectFromStorage = (rank, newObject) => {
+
+    localStorage.removeItem(`Product${rank}`);
+    localStorage.setItem(`Product${rank}`, JSON.stringify(newObject));
+};
+
+
+const increaseQuantityOfProductInCart = product => {
+
+    const productRank = isTheProductAlreadyInTheCartAndWhatIsTheRank(product);
+    const productInStorage = getProductJsonFromStorage(productRank);
+    const newQuantity = addNumbersFromString(productInStorage.quantity, productQuantity);
+
+    productInStorage.quantity = newQuantity;
+
+    replaceObjectFromStorage(productRank, productInStorage);
+};
+
+
+const addNewProductToCart = product => {
+
+    const productRank = localStorage.length + 1;
+    const productLinea = JSON.stringify(product);
+
+    localStorage.setItem(`Product${productRank}`, productLinea);
+};
+
+
+const putProductInCart = () => {
+
+    const product = getCurrentProduct();
+
+    if(isTheProductComplete()) {
+
+        if(isTheProductAlreadyInTheCartAndWhatIsTheRank(product)) {
+
+            increaseQuantityOfProductInCart(product);
+
+        } else {
+
+            addNewProductToCart(product);
+        };
+    } else {
+
+        console.log('Please choose a color and a quantity.');
+    };
+};
+
+
 const main = async () => {
 
     const productId = await getProductIdFromUrl();
     const product = await retrieveProduct(productId);
 
     createProductInfo(product);
+    
+    const colorSelect = document.getElementById('colors');
+    const quantitySelect = document.getElementById('quantity');
+    const button = document.getElementById('addToCart');
+
+    colorSelect.addEventListener('change', function(e) {
+
+        productColor = e.target.value;
+    });
+    quantitySelect.addEventListener('input', function(e) {
+
+        productQuantity = e.target.value;
+    });
+    button.addEventListener("click", putProductInCart);
 };
 
 

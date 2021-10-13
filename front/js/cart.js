956d591a3c2f25405d7efb34e7c2a9b74c4e4840
Estelle Maillet <estellemaillet00@gmail.com>
2021-10-13 16:48:19 +0200

    Finition de l etape 9 : gerer la modification et la suppression des produits dans la page Panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,17 @@
-/*récupérer les procduit dans localStorage
-pour chaque produit récupérer les infos depuis l'API
-pour chaque info, créer le bon truc sur la page */
-
-const retrieveProduct = (id) => fetch(`http://localhost:3000/api/products/${id}`)
+const retrieveProductListApi = () => fetch("http://localhost:3000/api/products")
 .then(res => res.json())
-.catch(err => console.log('Erreur retrieveProduct', err));
+.catch(err => console.log("Erreur retrieveProductListApi", err));
+
+
+const retrieveProductApi = id => fetch(`http://localhost:3000/api/products/${id}`)
+.then(res => res.json())
+.catch(err => console.log('Erreur retrieveProductApi', err));
+
+
+const retrieveProductStorage = rank => {
+
+    return JSON.parse(localStorage.getItem(`Product${rank}`));
+};
 
 
 const createProductCard = product => {
@@ -68,22 +75,6 @@ const createContentSettings = () => {
 };
 
 
-/* const retrieveQuantityFromId = rank => {
-
-    for(let i=0; i<localStorage.length; i++) {
-
-        const productRank = i + 1;
-        const product = JSON.parse(localStorage.getItem(`Product${productRank}`));
-        const productId = product.id;
-
-        if(productId == id) {
-
-            return product.quantity;
-        };
-    };
-}; */
-
-
 const createContentSettingsQuantity = rank => {
 
     const contentSettingsQuantity = document.createElement("div");
@@ -99,11 +90,10 @@ const createContentSettingsQuantity = rank => {
     quantityInput.setAttribute("min", "1");
     quantityInput.setAttribute("max", "100");
 
-    const productStorage = JSON.parse(localStorage.getItem(`Product${rank}`))
+    const productStorage = retrieveProductStorage(rank);
     const productQuantity = productStorage.quantity;
 
     quantityInput.setAttribute("value", `${productQuantity}`);
-
     contentSettingsQuantity.appendChild(quantity);
     contentSettingsQuantity.appendChild(quantityInput);
 
@@ -126,37 +116,189 @@ const createContentSettingsDelete = () => {
 };
 
 
-const createProductInfo = (product, rank) => {
+const calculateTotalQuantity = () => {
 
-    const cartItemPlace = document.getElementById("cart__items");
-    const productCard = createProductCard(product);
-    const productImage = createProductImg(product);
-    const itemContent = createItemContent();
-    const titlePrice = createProductTitlePrice(product);
-    const contentSettings = createContentSettings();
-    const contentSettingsQuantity = createContentSettingsQuantity(rank);
-    const contentSettingsDelete = createContentSettingsDelete();
+    let totalQuantity = 0;
 
-    cartItemPlace.appendChild(productCard);
-    productCard.appendChild(productImage);
-    productCard.appendChild(itemContent);
-    itemContent.appendChild(titlePrice);
-    itemContent.appendChild(contentSettings);
-    contentSettings.appendChild(contentSettingsQuantity);
-    contentSettingsQuantity.appendChild(contentSettingsDelete);
+    for(let i = 0; i < localStorage.length; i++) {
+
+        const rank = i + 1;
+        const productStorage = retrieveProductStorage(rank);
+
+        totalQuantity += parseInt(productStorage.quantity, 10);
+    };
+
+    return totalQuantity;
+};
+
+
+const displayTotalQuantity = () => {
+
+    const totalQuantityPlace = document.getElementById("totalQuantity");
+    totalQuantityPlace.innerText = calculateTotalQuantity();
+
+    return totalQuantityPlace;
+};
+
+
+const calculateTotalPrice = async () => {
+
+    let totalPrice = 0;
+
+    for(let i = 0; i < localStorage.length; i++) {
+
+        const rank = i + 1;
+        const productStorage = retrieveProductStorage(rank);
+        const product = await retrieveProductApi(productStorage.id);
+
+        totalPrice += parseInt(productStorage.quantity, 10) * parseFloat(product.price);
+    };
+
+    return totalPrice;
+};
+
+
+const displayTotalPrice = async () => {
+
+    const totalPricePlace = document.getElementById("totalPrice");
+    totalPricePlace.innerText = await calculateTotalPrice();
+
+    return totalPricePlace;
+};
+
+
+const createProductInfo = async () => {
+
+    for(let i = 0; i < localStorage.length; i++) {
+
+        const rank = i + 1;
+        const productStorage = retrieveProductStorage(rank);
+        const product = await retrieveProductApi(productStorage.id);
+
+        if(productStorage.quantity != 0){ 
+
+            const cartItemPlace = document.getElementById("cart__items");
+            const productCard = createProductCard(product);
+            const productImage = createProductImg(product);
+            const itemContent = createItemContent();
+            const titlePrice = createProductTitlePrice(product);
+            const contentSettings = createContentSettings();
+            const contentSettingsQuantity = createContentSettingsQuantity(rank);
+            const contentSettingsDelete = createContentSettingsDelete();
+
+            cartItemPlace.appendChild(productCard);
+            productCard.appendChild(productImage);
+            productCard.appendChild(itemContent);
+            itemContent.appendChild(titlePrice);
+            itemContent.appendChild(contentSettings);
+            contentSettings.appendChild(contentSettingsQuantity);
+            contentSettingsQuantity.appendChild(contentSettingsDelete);
+        };
+    };
+
+    displayTotalQuantity();
+    await displayTotalPrice();
+};
+
+/* *
+*Find a product and its rank in localStorage from known id and quantity of product
+*@param {string} id
+*@param {interger|string} quantity
+*@return {oject} product, {interger} rank
+*/
+const findProductAndRankInStorageFromIdAndQuantity = (id, quantity) => {
+
+    let product;
+    let rank;
+
+    for(let i = 0; i < localStorage.length; i++) {
+
+        const productRank = i + 1;
+        const productStorage = retrieveProductStorage(productRank);
+
+        if(id == productStorage.id && quantity == productStorage.quantity){
+
+            product = productStorage;
+            rank = productRank;
+        };
+    };
+
+    return [product, rank];
+};
+
+
+const replaceProductInStorage = (rank, product) => {
+
+    localStorage.removeItem(`Product${rank}`);
+    localStorage.setItem(`Product${rank}`, JSON.stringify(product));
+};
+
+
+const addEventListenerToQuantityInput = () => {
+
+    const inputList = document.querySelectorAll(".itemQuantity");
+
+    for(let input of inputList) {
+
+        const productArticle = input.closest("article.cart__item");
+        const quantityOfProduct = input.value;
+        const productId = productArticle.dataset.id;
+        const product = findProductAndRankInStorageFromIdAndQuantity(productId, quantityOfProduct)[0];
+        const rank = findProductAndRankInStorageFromIdAndQuantity(productId, quantityOfProduct)[1];
+
+        input.addEventListener("change", async (e) => {
+
+            const newQuantity = e.target.value;
+
+            product.quantity = newQuantity;
+
+            replaceProductInStorage(rank, product);
+
+            if (newQuantity == 0) {
+
+                productArticle.remove();
+            };
+
+            displayTotalQuantity();
+            await displayTotalPrice();
+        });
+    };
+};
+
+
+const addEventListenerToDeleteButton = () => {
+
+    const buttonList = document.querySelectorAll("p.deleteItem");
+
+    for(let button of buttonList) {
+
+        const productArticle = button.closest("article.cart__item");
+        const quantityInput = productArticle.querySelector("input.itemQuantity")
+        const quantityOfProduct = quantityInput.value;
+        const productId = productArticle.dataset.id;
+        const product = findProductAndRankInStorageFromIdAndQuantity(productId, quantityOfProduct)[0];
+        const rank = findProductAndRankInStorageFromIdAndQuantity(productId, quantityOfProduct)[1];
+
+        button.addEventListener("click", async () => {
+
+            product.quantity = 0;
+
+            replaceProductInStorage(rank, product);
+
+            productArticle.remove();
+
+            displayTotalQuantity();
+            await displayTotalPrice();
+        });
+    };
 };
 
 
 const main = async () => {
 
-    for(let i=0; i < localStorage.length; i++) {
-
-        const productRank = i + 1;
-        const productStorage = JSON.parse(localStorage.getItem(`Product${productRank}`));
-        const product = await retrieveProduct(productStorage.id);
-
-        createProductInfo(product, productRank);
-    } 
+    await createProductInfo();
+    addEventListenerToQuantityInput();
+    addEventListenerToDeleteButton();
 };
 
 main();
